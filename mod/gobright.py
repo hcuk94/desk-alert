2fb40b501088368a12f4acb491ab1c93d5a66c5e
hcuk94 <me@hc.me.uk>
2022-08-16 16:10:08 +0100

    some idle work

--- mod/gobright.py
+++ mod/gobright.py
@@ -1,25 +1,40 @@
 import requests
+import datetime
 
 
 class GoBright:
     """Hello!"""
-    def __init__(self, api_endpoint, api_auth):
-        self.api_endpoint = api_endpoint
+    def __init__(self, api_auth, location_id, date_time, duration):
+        self.api_baseurl = "https://t1b.gobright.cloud/api/v2.0/"
         self.api_auth = api_auth
+        self.location_id = location_id
+        self.date_time = date_time
+        self.duration = duration * 60
 
     def availability(self):
-        req_url = self.api_endpoint + "spaces/desks/availability"
+        req_url = self.api_baseurl + "spaces/desks/availability"
         req_head = {
             'Authorization': self.api_auth
         }
         req_body = {
               "timeZone": "Europe/London",
-              "locationId": "bb0527a8-0c37-4580-bdac-092c7df2249d",
-              "dateTime": "2022-08-18 08:00",
-              "duration": 480,
+              "locationId": self.location_id,
+              "dateTime": self.date_time,
+              "duration": self.duration,
               "spaceType": 1,
               "capacity": 1,
         }
         req = requests.post(req_url, headers=req_head, json=req_body)
         req_resp = req.json()
         return req_resp
+
+    def num_desks(self):
+        avail = self.availability()
+        locations = avail['data']['locations']
+        count_loc = len(locations)
+        count_desk = 0
+        if count_loc > 0:
+            for location in locations:
+                count_desk += len(location['spaces'])
+        return count_desk
+
--- scratch.py
+++ scratch.py
@@ -6,5 +6,8 @@ import logging
 from time import sleep
 import requests
 
-test = gobright.GoBright(config.gobright_api, config.auth_cookie)
-print(test.availability())
+date_time = "2022-08-16 08:00"
+duration = 8
+
+test = gobright.GoBright(config.gb_auth, config.gb_location, date_time, duration)
+print(test.num_desks())
